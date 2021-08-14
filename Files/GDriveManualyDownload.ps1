a01fee4f0cd69112fe24f18ac80f225dc22133f1
THrownCreeper <55951719+THrownCreeper@users.noreply.github.com>
2021-08-14 20:20:56 +0200

    Add files via upload

---
+++ Files/GDriveManualyDownload.ps1
@@ -0,0 +1,22 @@
+Start-Process -FilePath chrome.exe -ArgumentList "https://drive.google.com/u/0/uc?export=download&confirm=7nyL&id=1fdvoj8loqsl5UrpESZTs-S0Y6bBS6Z0A"
+Start-Sleep -s 5
+$i = 0;
+$times = 13;
+for (;;) {
+    if ($i -le  $times) {
+        Write-Host($i)
+        $wsh = New-Object -ComObject WScript.Shell
+            $wsh.SendKeys('{TAB}')
+    }
+    if ($i -eq $times) {
+        break
+    }
+    $i++
+    Start-Sleep -s 1
+}
+Write-Host("Tab szor" + $i) 
+Write-Host("") 
+Write-Host("Enter")
+Start-Sleep -s 1
+$wsh = New-Object -ComObject WScript.Shell
+    $wsh.SendKeys('{ENTER}')
