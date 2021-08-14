728b60377506202fcf51fb934f1896a1ddc1142f
THrownCreeper <55951719+THrownCreeper@users.noreply.github.com>
2021-08-14 20:47:49 +0200

    Update GDriveManualyDownload.ps1

--- Files/GDriveManualyDownload.ps1
+++ Files/GDriveManualyDownload.ps1
@@ -1,7 +1,8 @@
+Start-Sleep -s 20
 Start-Process -FilePath chrome.exe -ArgumentList "https://drive.google.com/u/0/uc?export=download&confirm=7nyL&id=1fdvoj8loqsl5UrpESZTs-S0Y6bBS6Z0A"
 Start-Sleep -s 5
 $i = 0;
-$times = 13;
+$times = 12;
 for (;;) {
     if ($i -le  $times) {
         Write-Host($i)
