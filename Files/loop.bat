7d7efcc5e8560b83b09959d8ae2e6b80dfd94c82
THrownCreeper <55951719+THrownCreeper@users.noreply.github.com>
2021-08-14 11:43:59 +0200

    Update loop.bat

--- Files/loop.bat
+++ Files/loop.bat
@@ -1,5 +1,5 @@
-tasklist | find /i "ngrok.exe" && goto check || echo "NGROK Services is not Running. One NGROK FREE Account/ 1 Tunnel, If u want Run more VM, Update new NGROK_AUTH_TOKEN into Settings> Secrets> Repository secrets then run workflow again. Your current workflow end shortly. If you still have questions contact: https://fb.com/404 " & ping 127.0.0.1 & ping 127.0.0.1 & exit
+tasklist | find /i "cmd.exe" && goto check || echo "NGROK Services is not Running. One NGROK FREE Account/ 1 Tunnel, If u want Run more VM, Update new NGROK_AUTH_TOKEN into Settings> Secrets> Repository secrets then run workflow again. Your current workflow end shortly. If you still have questions contact: https://fb.com/404 " & ping 127.0.0.1 & ping 127.0.0.1 & exit
 :check
-ping 127.0.0.1 > nul
+ping 127.0.0.1
 cls
 goto check
