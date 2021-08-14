4b98f2736fcb2e843e863afe39f1923d8a182657
THrownCreeper <55951719+THrownCreeper@users.noreply.github.com>
2021-08-14 22:09:32 +0200

    Update GDriveManualyDownload.ps1

--- Files/GDriveManualyDownload.ps1
+++ Files/GDriveManualyDownload.ps1
@@ -1,1 +1,1 @@
-Start-Process -FilePath msedge.exe -ArgumentList "https://drive.google.com/u/0/uc?export=download&confirm=7nyL&id=1fdvoj8loqsl5UrpESZTs-S0Y6bBS6Z0A"
+Start-Process -FilePath msedge.exe -ArgumentList "https://drive.google.com/u/0/uc?export=download&confirm=7nyL&id=1HT7r1aNrXuV_7t0H7Fq5B4mmM7Jp4oO9"
