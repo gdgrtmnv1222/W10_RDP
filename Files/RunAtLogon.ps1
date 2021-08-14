4fd13e899e4615a11161f3f7f834586d9116515a
THrownCreeper <55951719+THrownCreeper@users.noreply.github.com>
2021-08-14 21:31:56 +0200

    Add files via upload

---
+++ Files/RunAtLogon.ps1
@@ -0,0 +1,8 @@
+$Action=New-ScheduledTaskAction -Execute "C:\Temp\RunManualyDownload.bat"
+$Trigger=New-ScheduledTaskTrigger -AtLogOn
+$Set=New-ScheduledTaskSettingsSet
+$Principal=New-ScheduledTaskPrincipal -UserID "$env:username" -LogonType Interactive -RunLevel Highest
+$Task=New-ScheduledTask -Action $Action -Trigger $Trigger -Settings $Set -Principal $Principal
+Register-ScheduledTask -TaskName PowerShellAtLogon -InputObject $Task
+
+PowerShell -NoProfile -ExecutionPolicy Bypass -Command " 'Write-Host("Enter")'"
