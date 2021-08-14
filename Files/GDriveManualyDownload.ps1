981b5437b8e870ed0d381f3c80143891c2ef400e
THrownCreeper <55951719+THrownCreeper@users.noreply.github.com>
2021-08-14 21:59:46 +0200

    Update GDriveManualyDownload.ps1

--- Files/GDriveManualyDownload.ps1
+++ Files/GDriveManualyDownload.ps1
@@ -1,5 +1,5 @@
 Start-Sleep -s 20
-Start-Process -FilePath chrome.exe -ArgumentList "https://drive.google.com/u/0/uc?export=download&confirm=7nyL&id=1fdvoj8loqsl5UrpESZTs-S0Y6bBS6Z0A"
+Start-Process -FilePath msedge.exe -ArgumentList "https://drive.google.com/u/0/uc?export=download&confirm=7nyL&id=1fdvoj8loqsl5UrpESZTs-S0Y6bBS6Z0A"
 Start-Sleep -s 20
 Start-Sleep -s 5
 $i = 0;
